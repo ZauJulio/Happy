efff07a71381f0b0bf03482e5327ee95d4a321c3
ZauJulio <zauhdf@gmail.com>
2020-10-14 17:46:11 -0300

    feature: add views

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -1,22 +1,27 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import Orphanage from "../models/Orphanage";
+import orphanageView from "../views/orphanages_view";
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ["images"],
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ["images"],
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
@@ -33,9 +38,9 @@ export default {
     const orphanagesRepository = getRepository(Orphanage);
 
     const requestImages = request.files as Express.Multer.File[];
-    const images = requestImages.map(image => {
-      return { path: image.filename }
-    })
+    const images = requestImages.map((image) => {
+      return { path: image.filename };
+    });
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
+import path from 'path';
+
 import './database/connection';
 
 import routes from './routes';
@@ -7,5 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
 
 app.listen(3333);
